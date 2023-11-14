9026f7f01827f0393f14134ab3ef8489be1f12a8
Оля М <olsermi@outlook.com>
2023-11-14 23:54:30 +0300

    src/main/java/Epic.java

--- src/main/java/Epic.java
+++ src/main/java/Epic.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Epic {
+package ru.netology;
+
+public class Epic extends Task {
+
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        for (String subtask : subtasks) {
+            if (subtask.contains(query)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
--- src/main/java/SimpleTask.java
+++ src/main/java/SimpleTask.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class SimpleTask {
+package ru.netology;
+
+public class SimpleTask extends Task {
+
+    protected String title;
+
+    public SimpleTask(int id, String title) {
+        super(id);
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public boolean matches(String query) {
+        if (title.contains(query)) {
+            return true;
+        }
+        return false;
+    }
+
 }
